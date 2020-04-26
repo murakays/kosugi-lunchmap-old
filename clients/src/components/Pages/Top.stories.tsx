b182486730e1555dc12ae32522d8cad0ca0d63f7
murakays <discenstar321@gmail.com>
2020-04-26 14:27:55 +0900

    storybook導入
    [ci skip]

---
+++ clients/src/components/Pages/Top.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Top from './Top';
+
+export default {
+  title: 'Top',
+};
+
+export const TopComponent = () => <Top />;
--- clients/webpack.common.js
+++ clients/webpack.common.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   plugins: [
     new htmlWebpackPlugin({
-      template: 'index.html',
+      template: './src/index.html',
     }),
   ],
 };
