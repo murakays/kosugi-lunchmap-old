2ec2f8877a39450a050b0ff7f5993f89d02d2a11
murakays <discenstar321@gmail.com>
2020-05-03 11:05:11 +0900

    headerとfooterの領域確保

--- clients/src/components/App.tsx
+++ clients/src/components/App.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { history, store } from '@/store/configureStore';
+import Header from '@/components/Organism/Header';
+import Footer from '@/components/Organism/Footer';
 
 // Components
-import Top from '@/components/Pages/Top';
+const Top = React.lazy(() => import('@/components/Pages/Top'));
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Switch>
-          <Route exact path="/" component={Top} />
-        </Switch>
+        <Header />
+        <Suspense fallback={<div>loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Top} />
+          </Switch>
+        </Suspense>
+        <Footer />
       </ConnectedRouter>
     </Provider>
   );
