553146af5ba9b19042b3de94227f77b963d1dfdc
murakays <discenstar321@gmail.com>
2020-05-11 23:30:37 +0900

    Google認証ログインロジック追加&Headerにログイン、ログアウトボタンを追加

--- clients/src/components/App.tsx
+++ clients/src/components/App.tsx
@@ -1,25 +1,14 @@
-import React, { Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { history, store } from '@/store/configureStore';
-import Header from '@/components/Organism/Header';
-import Footer from '@/components/Organism/Footer';
-
-// Components
-const Top = React.lazy(() => import('@/components/Pages/Top'));
+import Routes from '@/components/Routes';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Header />
-        <Suspense fallback={<div>loading...</div>}>
-          <Switch>
-            <Route exact path="/" component={Top} />
-          </Switch>
-        </Suspense>
-        <Footer />
+        <Routes />
       </ConnectedRouter>
     </Provider>
   );
--- clients/src/components/Organism/Header.tsx
+++ clients/src/components/Organism/Header.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { jsx } from '@emotion/core';
 import styled from '@emotion/styled';
+import { FirebaseUser, firebaseLogin, firebaseLogout } from '@/lib/firebase';
+import { useDispatch } from 'react-redux';
 
 const HeaderStyle = styled.header`
   min-width: 980px;
@@ -10,25 +12,71 @@ const HeaderStyle = styled.header`
   display: flex;
 `;
 
-// 今後firebaseログアウト処理をいれる
-const LoginLink = styled.span`
-  color: blue;
-  margin: 0 0 0 auto;
-`;
+type UseHooksProps = {
+  login: () => void;
+  logout: () => void;
+};
+const useHooksProps = (): UseHooksProps => {
+  const dispatch = useDispatch();
+  return {
+    login: React.useCallback(() => {
+      dispatch(firebaseLogin());
+    }, [dispatch]),
+    logout: React.useCallback(() => {
+      dispatch(firebaseLogout());
+    }, [dispatch]),
+  };
+};
 
-const Header: React.FC = () => {
+type IProps = {
+  user: FirebaseUser;
+};
+type IHeader = IProps & UseHooksProps;
+
+const Header: React.FC<IHeader> = (props: IHeader) => {
   const TitleLogo = styled.div`
     width: 500px;
     height: 40px;
     background-color: blue;
   `;
+  const { user, login, logout } = props;
 
+  // ログイン用リンクはGoogleの公式バナーを用いる
   return (
     <HeaderStyle>
       <TitleLogo />
-      <LoginLink>ログイン</LoginLink>
+      {user ? (
+        <div>
+          <span>{user?.displayName || 'guest'}</span>
+          <a
+            href=""
+            onClick={(event: React.MouseEvent<HTMLAnchorElement>): void => {
+              event.preventDefault();
+              logout();
+            }}
+          >
+            ログアウト
+          </a>
+        </div>
+      ) : (
+        <div>
+          <span>ログインしていません(バナーを使う）</span>
+          <a
+            href=""
+            onClick={(event: React.MouseEvent<HTMLAnchorElement>): void => {
+              event.preventDefault();
+              login();
+            }}
+          >
+            Googleアカウントでログインする
+          </a>
+        </div>
+      )}
     </HeaderStyle>
   );
 };
 
-export default Header;
+export default function HeaderContainer(props: IProps): JSX.Element {
+  const _props = { ...props, ...useHooksProps() };
+  return <Header {..._props} />;
+}
