aef2c21d2e468a0884a75fb72528966a22d14f0c
murakays <discenstar321@gmail.com>
2020-06-22 23:03:05 +0900

    material-ui 一覧のスタイル修正&画面render時に情報取得する用に実装

--- clients/src/components/Organism/RestaurantListsCard.tsx
+++ clients/src/components/Organism/RestaurantListsCard.tsx
@@ -9,36 +9,55 @@ import { mediaSize } from '@/const/restaurant';
 import Card from '@material-ui/core/Card';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 // styles
 const CardStyle = css`
-  width: 980px;
+  max-width: 984px;
   margin-left: auto;
   margin-right: auto;
 `;
 
+const MediaCardStyle = css`
+  width: 320px;
+`;
+
+const CircularProgressStyle = css`
+  width: 40px;
+  height: 40px;
+  margin: auto;
+  padding: 100px;
+`;
+
 type IProps = {
   restaurantInfo: restaurantInfo[];
+  isGetting: boolean;
 };
 
 export const RestaurantListsCard: React.FC<IProps> = (props: IProps) => {
-  const { restaurantInfo } = props;
+  const { restaurantInfo, isGetting } = props;
   return (
     <Card css={CardStyle}>
-      <GridList cellHeight={300} cols={3}>
-        {restaurantInfo.map((value, index) => {
-          return (
-            <GridListTile key={index}>
-              <MediaCard
-                area={value.area}
-                mediaUrl={value.mediaUrl}
-                mediaSize={mediaSize.lists}
-                restaurantName={value.restaurantName}
-              />
-            </GridListTile>
-          );
-        })}
-      </GridList>
+      {isGetting ? (
+        <div css={CircularProgressStyle}>
+          <CircularProgress />
+        </div>
+      ) : (
+        <GridList cellHeight={300} cols={3}>
+          {restaurantInfo.map((value, index) => {
+            return (
+              <GridListTile key={index} css={MediaCardStyle}>
+                <MediaCard
+                  area={value.area}
+                  mediaUrl={value.mediaUrl}
+                  mediaSize={mediaSize.lists}
+                  restaurantName={value.restaurantName}
+                />
+              </GridListTile>
+            );
+          })}
+        </GridList>
+      )}
     </Card>
   );
 };
--- clients/src/components/Pages/Top.tsx
+++ clients/src/components/Pages/Top.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/store/configureStore';
 import { RestaurantState, getRestaurantInfo } from '@/modules/restaurantList';
@@ -24,49 +24,21 @@ type Props = ReturnType<typeof useHooksProps>;
 export const Top: React.FC<Props> = (props: Props) => {
   const { isGetting, restaurantInfo, page, limit, getRestaurantList } = props;
 
+  useEffect(() => {
+    getRestaurantList();
+  }, []);
+
   return (
     <div>
-      {isGetting && <span>isGetting...</span>}
-      <button onClick={getRestaurantList}>fetch</button>
       <p>Page:{page}</p>
       <p>Limit:{limit}</p>
-      <RestaurantListsCard restaurantInfo={restaurantInfo} />
+      <RestaurantListsCard restaurantInfo={restaurantInfo} isGetting={isGetting} />
     </div>
   );
 };
 
-// props mock
-const mock = {
-  isGetting: false,
-  restaurantInfo: [
-    {
-      restaurantId: 1,
-      restaurantName: 'デニーズ',
-      area: 'NE',
-      mediaUrl:
-        'https://firebasestorage.googleapis.com/v0/b/kosugi-lunchmap.appspot.com/o/public%2Frestaurants%2F1%2FIBUS458A1871_TP_V4.jpg?alt=media',
-    },
-    {
-      restaurantId: 2,
-      restaurantName: 'さくら水産',
-      area: 'NE',
-      mediaUrl:
-        'https://firebasestorage.googleapis.com/v0/b/kosugi-lunchmap.appspot.com/o/public%2Frestaurants%2F1%2FIBUS458A1871_TP_V4.jpg?alt=media',
-    },
-    {
-      restaurantId: 3,
-      restaurantName: '山田うどん',
-      area: 'NE',
-      mediaUrl:
-        'https://firebasestorage.googleapis.com/v0/b/kosugi-lunchmap.appspot.com/o/public%2Frestaurants%2F1%2FIBUS458A1871_TP_V4.jpg?alt=media',
-    },
-  ],
-  limit: 10,
-  page: 1,
-};
-
 // container Component
 export default function TopContainer(): JSX.Element {
-  const _props = { ...useHooksProps(), ...mock };
+  const _props = { ...useHooksProps() };
   return <Top {..._props} />;
 }
